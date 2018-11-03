46d2436e7f2a8c170db5db7c83ad6fbd7c1a0dc3
Omar Naeem <omar_naeem@live.com>
2018-11-03 12:28:32 +0500

    Redux added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
 import './index.css';
 import LoginPage from './pages/Login';
 import ChatPage from './pages/Chat';
 import { Router, Route, Redirect } from "react-router";
 import createBrowserHistory from "history/createBrowserHistory";
+import { createStore } from 'redux';
+import { MessagesList } from './redux/reducers';
 
+const store = createStore(MessagesList);
 const history = createBrowserHistory();
 
 class App extends Component {
   render() {
     return (
-      <Router history={history}>
-        <div className="App" style={{
-          height: '100%',
-          width: '100%',
-          display: 'flex'
-        }}>
-          <Route exact path={'/login'} component={LoginPage} />
-          <Route exact path={'/home'} component={ChatPage} />
-          <Redirect to={'/login'} />
-        </div>
-      </Router>
+      <Provider store={store} >
+        <Router history={history}>
+          <div className="App" style={{
+            height: '100%',
+            width: '100%',
+            display: 'flex'
+          }}>
+            <Route exact path={'/login'} component={LoginPage} />
+            <Route exact path={'/home'} component={ChatPage} />
+            <Redirect to={'/login'} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
